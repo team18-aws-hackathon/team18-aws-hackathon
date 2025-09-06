90d870764d5cc14464b9cdc70830fb42ac0f28c0
jungeun5-choi <jungeun5.choi@gmail.com>
2025-09-06 13:00:11 +0900

    refactor: dynamo db의 deprecated 경고 수정

--- 2. IaC/backend.tf
+++ 2. IaC/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "team18-terraform-state-740d78b6"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "team18-terraform-locks"
-    encrypt        = true
+    bucket       = "team18-terraform-state-740d78b6"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
