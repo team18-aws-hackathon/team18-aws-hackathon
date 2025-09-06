1ef9dc231b045137526d751e38a1848befca6d35
jungeun5-choi <jungeun5.choi@gmail.com>
2025-09-06 11:11:38 +0900

    feat: configure Terraform remote state backend
    # terraform output terraform_state_bucket
    # terraform output terraform_locks_table
    # backend.tf 수정
    # terraform init -migrate-state

---
+++ 2. IaC/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "team18-terraform-state-740d78b6"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "team18-terraform-locks"
+    encrypt        = true
+  }
+}
